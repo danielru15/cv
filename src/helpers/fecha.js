eedafbe2ae18da2be617d5b44dd9b21441ad905f
Danielru <danielru@MacBook-Pro-de-Daniel.local>
2024-02-05 13:06:55 -0500

    faltan comandos teclado

---
+++ src/helpers/fecha.js
@@ -0,0 +1,8 @@
+export const obtenerFecha = (startDate,endDate ) => {
+    const startMonthYear = new Date(startDate).toLocaleString('default', {year:"numeric",  month: 'short' })
+    const endMonthYear = endDate != null ? new Date(endDate).toLocaleString('default', {year:"numeric", month: 'short' }) :  "Actual" 
+    const yearMonth = `${startMonthYear}  - ${endMonthYear}`
+    
+    return  yearMonth  
+}
+
