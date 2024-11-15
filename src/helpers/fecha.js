ecdf550105f4a7f674eca322ab8a799558b8bbb6
Danielru <danielru@192.168.1.9>
2024-11-14 19:17:21 -0500

    2024-1

--- src/helpers/fecha.js
+++ src/helpers/fecha.js
@@ -1,6 +1,6 @@
 export const obtenerFecha = (startDate,endDate ) => {
     const startMonthYear = new Date(startDate).toLocaleString('default', {year:"numeric",  month: 'short' })
-    const endMonthYear = endDate != null ? new Date(endDate).toLocaleString('default', {year:"numeric", month: 'short' }) :  "Actual" 
+    const endMonthYear = endDate ? new Date(endDate).toLocaleString('default', {year:"numeric", month: 'short' }) :  "Actual" 
     const yearMonth = `${startMonthYear}  - ${endMonthYear}`
     
     return  yearMonth  
